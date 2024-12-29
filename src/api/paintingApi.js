e9932a112f4a48e4b698f47c5041d559f6da8c32
Daniel Krastev <danielkrastev01@gmail.com>
2024-12-29 23:24:08 +0200

    Add new function for get sort paintings

--- src/api/paintingApi.js
+++ src/api/paintingApi.js
@@ -24,7 +24,20 @@ async function getOne(id) {
     return result;
 }
 
+async function getSort(bySort) {
+    const response = await fetch(`${baseUrl}.json?orderBy="${bySort}"`);
+
+    if(!response.ok){
+        return await new Error(response.json());
+    }
+
+    const result = await response.json();
+
+    return result;
+}
+
 export default{
     getAll,
-    getOne
+    getOne,
+    getSort
 }
