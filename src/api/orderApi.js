ac2f78703685c42666533ee7dae59b79aa0defe4
Daniel Krastev <danielkrastev01@gmail.com>
2025-01-19 13:07:36 +0200

    Create order api

---
+++ src/api/orderApi.js
@@ -0,0 +1,44 @@
+const baseUrl = 'https://painting-shop-krasteva-default-rtdb.europe-west1.firebasedatabase.app/orders';
+
+async function getAll() {
+    const response = await fetch(`${baseUrl}.json`);
+    
+    if(!response.ok){
+        return await new Error(response.json());
+    }
+
+    const result = await response.json();
+
+    return Object.values(result);
+}
+
+async function getOne(id) {
+    const response = await fetch(`${baseUrl}/${id}.json`);
+
+    if(!response.ok){
+        return await new Error(response.json());
+    }
+
+    const result = await response.json();
+
+    return Object.values(result);
+}
+
+async function create(data, token) {
+    const response = await fetch(`${baseUrl}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    return result;
+}
+
+export default{
+    getAll,
+    getOne,
+    create
+}
